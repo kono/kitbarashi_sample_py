02524b948b92264c10209496817babbb93761394
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-28 15:55:48 +0900

    Add kitmaster.py

--- kitbarashi.py
+++ kitbarashi.py
@@ -1,3 +1,5 @@
+import kitmaster
+
 class Kitbarashi:
     def barashi(self, item, qty, price):
         b ={"item":item, "qty":qty, "price":price}
---
+++ kitmaster.py
@@ -0,0 +1,8 @@
+class Kitmaster:
+    def kitprice(self):
+        data = [
+        {"kitcode":2, "price_ar":[120,110,100,0]},
+        {"kitcode":9, "price_ar":[200,230,250,210]}
+        ]
+        return data
+
