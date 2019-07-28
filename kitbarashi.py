9b3fbef1cc22e78503a739fabfebd6935d62b7c0
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-28 16:12:17 +0900

    Add Kitbarashi.kitmaster and Kitbarashi.kittable

--- kitbarashi.py
+++ kitbarashi.py
@@ -1,6 +1,11 @@
 import kitmaster
 
 class Kitbarashi:
+    def __init__(self):
+        master = kitmaster.Kitmaster()
+        self.kitprice = master.kitprice
+        self.kittable = master.kittable
+
     def barashi(self, item, qty, price):
         b ={"item":item, "qty":qty, "price":price}
         return [b]
