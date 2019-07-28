376f09aa208ecf8158a67d48ce43a226307a900e
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-28 16:09:24 +0900

    Fix kitmaster code

--- kitmaster.py
+++ kitmaster.py
@@ -8,11 +8,12 @@ class Kitmaster:
     
     def kittable(self):
         data = [
-            {"kitcode"=>2, "linecode"=>10, "qty"=>1, "price_ar"=>[40,30,30,0]},
-            {"kitcode"=>2, "linecode"=>11, "qty"=>2, "price_ar"=>[20,20,20,0]},
-            {"kitcode"=>2, "linecode"=>12, "qty"=>1, "price_ar"=>[40,40,30,0]},
-            {"kitcode"=>9, "linecode"=>91, "qty"=>1, "price_ar"=>[120,130,140,120]},
-            {"kitcode"=>9, "linecode"=>92, "qty"=>1, "price_ar"=>[80,100,110,90]}
+            {"kitcode": 2, "linecode": 10, "qty": 1, "price_ar": [40,30,30,0]},
+            {"kitcode": 2, "linecode": 11, "qty": 2, "price_ar": [20,20,20,0]},
+            {"kitcode": 2, "linecode": 12, "qty": 1, "price_ar": [40,40,30,0]},
+            {"kitcode": 9, "linecode": 91, "qty": 1, "price_ar": [120,130,140,120]},
+            {"kitcode": 9, "linecode": 92, "qty": 1, "price_ar": [80,100,110,90]}
             ]
+        return data
 
 
