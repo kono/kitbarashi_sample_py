08080b113b28830a3146aa07f80859600812552d
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-30 21:21:10 +0900

    Use kitprice data to judge item

--- kitbarashi.py
+++ kitbarashi.py
@@ -1,11 +1,26 @@
 import kitmaster
 
 class Kitbarashi:
+    kitprice = (
+        {"kitcode":2, "price_ar":[120,110,100,0]},
+        {"kitcode":9, "price_ar":[200,230,250,210]}
+     )
+
     def __init__(self):
         master = kitmaster.Kitmaster()
-        self.kitprice = master.kitprice
         self.kittable = master.kittable
 
+    def iskit(self, item):
+        return_val = False
+        for r in self.kitprice:
+            if ("kitcode", item) in r.items():
+                return_val = True
+        return return_val
+
+
     def barashi(self, item, qty, price):
-        b ={"item":item, "qty":qty, "price":price}
+        b = False
+        a = self.iskit (item)
+        if  a == False:
+            b ={"item":item, "qty":qty, "price":price}
         return [b]
